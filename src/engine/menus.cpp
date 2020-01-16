0338221b8b4643d01e06321be1f132e20075bbd7
molexted <123afoster@gmail.com>
2020-01-15 22:01:09 -0800

    document menus.cpp

--- src/engine/menus.cpp
+++ src/engine/menus.cpp
@@ -1,3 +1,5 @@
+//automatic triggers for certain UIs in game
+
 #include "engine.h"
 
 void notifywelcome()
@@ -16,8 +18,14 @@ struct change
 static vector<change> needsapply;
 
 VARP(applydialog, 0, 1, 1);
+
+//when 0: change UI shows up whenever a pending change is added
+//when 1: change UI does not appear and applychanges must be used manually
 VAR(hidechanges, 0, 0, 1);
 
+//adds a change to the queue of settings changes,
+//if applydialog = 0 then this function does nothing
+//if hidechanges = 0 then this functin does not display changes UI at the end
 void addchange(const char *desc, int type)
 {
     if(!applydialog) return;
@@ -26,6 +34,7 @@ void addchange(const char *desc, int type)
     if(!hidechanges) UI::showui("changes");
 }
 
+//clears out pending changes added by addchange()
 void clearchanges(int type)
 {
     loopvrev(needsapply)
@@ -40,6 +49,7 @@ void clearchanges(int type)
     if(needsapply.empty()) UI::hideui("changes");
 }
 
+//goes through and applies changes that are enqueued
 void applychanges()
 {
     int changetypes = 0;
@@ -49,21 +59,26 @@ void applychanges()
     if(changetypes&CHANGE_SOUND) execident("resetsound");
 }
 
+//executes applychanges()
 COMMAND(applychanges, "");
+
+//returns if there are pending changes or not enqueued
 ICOMMAND(pendingchanges, "b", (int *idx), { if(needsapply.inrange(*idx)) result(needsapply[*idx].desc); else if(*idx < 0) intret(needsapply.length()); });
 
 static int lastmainmenu = -1;
 
+//used in main.cpp
 void menuprocess()
 {
     if(lastmainmenu != mainmenu)
     {
         lastmainmenu = mainmenu;
         execident("mainmenutoggled");
-    }    
+    }
     if(mainmenu && !isconnected(true) && !UI::hascursor()) UI::showui("main");
 }
 
+//toggles if the main menu is shown
 VAR(mainmenu, 1, 1, 0);
 
 void clearmainmenu()
