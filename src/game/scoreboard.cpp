82b939e1b83cfd6f89509588a0b9eaf5e66cdabc
Alex Foster <123afoster@gmail.com>
2020-05-01 14:34:41 -0700

    scoreboard.cpp style fixes

--- src/game/scoreboard.cpp
+++ src/game/scoreboard.cpp
@@ -24,40 +24,76 @@ namespace game
 
     void setteaminfo(int team, int frags)
     {
-        if(!VALID_TEAM(team)) return;
+        if(!VALID_TEAM(team))
+        {
+            return;
+        }
         teaminfo &t = teaminfos[team-1];
         t.frags = frags;
     }
 
+    //orders a pair of players' rank according to different criteria depending on mode
+    //spectators are ordered alphabetically
+    //ctf players are ordered by captures
+    //non-ctf is ordered by kills
     static inline bool playersort(const gameent *a, const gameent *b)
     {
         if(a->state==ClientState_Spectator)
         {
-            if(b->state==ClientState_Spectator) return strcmp(a->name, b->name) < 0;
-            else return false;
+            if(b->state==ClientState_Spectator)
+            {
+                return strcmp(a->name, b->name) < 0;
+            }
+            else
+            {
+                return false;
+            }
         }
-        else if(b->state==ClientState_Spectator) return true;
+        else if(b->state==ClientState_Spectator)
+        {
+            return true;
+        }
         if(modecheck(gamemode, Mode_CTF))
         {
-            if(a->flags > b->flags) return true;
-            if(a->flags < b->flags) return false;
+            if(a->flags > b->flags)
+            {
+                return true;
+            }
+            if(a->flags < b->flags)
+            {
+                return false;
+            }
         }
-        if(a->frags > b->frags) return true;
-        if(a->frags < b->frags) return false;
+        if(a->frags > b->frags)
+        {
+            return true;
+        }
+        if(a->frags < b->frags)
+        {
+            return false;
+        }
         return strcmp(a->name, b->name) < 0;
     }
 
+    //takes a vector of gameents and returns the one with the most kills
     void getbestplayers(vector<gameent *> &best)
     {
         for(int i = 0; i < players.length(); i++)
         {
             gameent *o = players[i];
-            if(o->state!=ClientState_Spectator) best.add(o);
+            if(o->state!=ClientState_Spectator)
+            {
+                best.add(o);
+            }
         }
         best.sort(playersort);
-        while(best.length() > 1 && best.last()->frags < best[0]->frags) best.drop();
+        while(best.length() > 1 && best.last()->frags < best[0]->frags)
+        {
+            best.drop();
+        }
     }
 
+    //taes a vector of team numbers and returns the one with the most frags
     void getbestteams(vector<int> &best)
     {
         if(cmode && cmode->hidefrags())
@@ -65,7 +101,11 @@ namespace game
             vector<teamscore> teamscores;
             cmode->getteamscores(teamscores);
             teamscores.sort(teamscore::compare);
-            while(teamscores.length() > 1 && teamscores.last().score < teamscores[0].score) teamscores.drop();
+            //loop through and drop teams
+            while(teamscores.length() > 1 && teamscores.last().score < teamscores[0].score)
+            {
+                teamscores.drop();
+            }
             for(int i = 0; i < teamscores.length(); i++)
             {
                 best.add(teamscores[i].team);
@@ -82,7 +122,10 @@ namespace game
             for(int i = 0; i < MAXTEAMS; ++i)
             {
                 teaminfo &t = teaminfos[i];
-                if(t.frags >= bestfrags) best.add(1+i);
+                if(t.frags >= bestfrags)
+                {
+                    best.add(1+i);
+                }
             }
         }
     }
@@ -99,8 +142,15 @@ namespace game
         for(int i = 0; i < players.length(); i++)
         {
             gameent *o = players[i];
-            if(!showconnecting && !o->name[0]) continue;
-            if(o->state==ClientState_Spectator) { spectators.add(o); continue; }
+            if(!showconnecting && !o->name[0])
+            {
+                continue;
+            }
+            if(o->state==ClientState_Spectator)
+            {
+                spectators.add(o);
+                continue;
+            }
             int team = modecheck(gamemode, Mode_Team) && VALID_TEAM(o->team) ? o->team : 0;
             teamplayers[team].add(o);
         }
@@ -124,12 +174,15 @@ namespace game
     {
         groupplayers();
     }
-
+//scoreboard commands
     COMMAND(refreshscoreboard, "");
     ICOMMAND(numscoreboard, "i", (int *team), intret(*team < 0 ? spectators.length() : (*team <= MAXTEAMS ? teamplayers[*team].length() : 0)));
     ICOMMAND(loopscoreboard, "rie", (ident *id, int *team, uint *body),
     {
-        if(*team > MAXTEAMS) return;
+        if(*team > MAXTEAMS)
+        {
+            return;
+        }
         LOOP_START(id, stack);
         vector<gameent *> &p = *team < 0 ? spectators : teamplayers[*team];
         for(int i = 0; i < p.length(); i++)
@@ -149,19 +202,28 @@ namespace game
             if(d->privilege)
             {
                 status = d->privilege>=Priv_Admin ? 0xFF8000 : 0x40FF80;
-                if(d->state==ClientState_Dead) status = (status>>1)&0x7F7F7F;
+                if(d->state==ClientState_Dead)
+                {
+                    status = (status>>1)&0x7F7F7F;
+                }
             }
             intret(status);
         }
     });
-
+    //scoreboard packet jump
     ICOMMAND(scoreboardpj, "i", (int *cn),
     {
         gameent *d = getclient(*cn);
         if(d && d != player1)
         {
-            if(d->state==ClientState_Lagged) result("LAG");
-            else intret(d->plag);
+            if(d->state==ClientState_Lagged)
+            {
+                result("LAG");
+            }
+            else
+            {
+                intret(d->plag);
+            }
         }
     });
 
@@ -170,11 +232,17 @@ namespace game
         gameent *d = getclient(*cn);
         if(d)
         {
-            if(!showpj && d->state==ClientState_Lagged) result("LAG");
-            else intret(d->ping);
+            if(!showpj && d->state==ClientState_Lagged)
+            {
+                result("LAG");
+            }
+            else
+            {
+                intret(d->ping);
+            }
         }
     });
-
+//scoreboard booleans
     ICOMMAND(scoreboardshowfrags, "", (), intret(cmode && cmode->hidefrags() && hidefrags ? 0 : 1));
     ICOMMAND(scoreboardshowclientnum, "", (), intret(showclientnum || player1->privilege>=Priv_Master ? 1 : 0));
     ICOMMAND(scoreboardmultiplayer, "", (), intret(multiplayer(false) || demoplayback ? 1 : 0));
@@ -184,16 +252,24 @@ namespace game
 
     ICOMMAND(scoreboardservinfo, "", (),
     {
-        if(!showservinfo) return;
+        if(!showservinfo)
+        {
+            return;
+        }
         const ENetAddress *address = connectedpeer();
         if(address && player1->clientnum >= 0)
         {
-            if(servdesc[0]) result(servdesc);
+            if(servdesc[0])
+            {
+                result(servdesc);
+            }
             else
             {
                 string hostname;
                 if(enet_address_get_host_ip(address, hostname, sizeof(hostname)) >= 0)
+                {
                     result(tempformatstring("%s:%d", hostname, address->port));
+                }
             }
         }
     });
@@ -213,7 +289,10 @@ namespace game
     {
         if(!modecheck(gamemode, Mode_Untimed) && getclientmap() && (maplimit >= 0 || intermission))
         {
-            if(intermission) result("intermission");
+            if(intermission)
+            {
+                result("intermission");
+            }
             else
             {
                 int secs = max(maplimit-lastmillis, 0)/1000;
@@ -226,11 +305,20 @@ namespace game
     {
         if(modecheck(gamemode, Mode_Team) && VALID_TEAM(*team))
         {
-            if(cmode && cmode->hidefrags()) intret(cmode->getteamscore(*team));
-            else intret(teaminfos[*team-1].frags);
+            if(cmode && cmode->hidefrags())
+            {
+                intret(cmode->getteamscore(*team));
+            }
+            else
+            {
+                intret(teaminfos[*team-1].frags);
+            }
         }
     });
 
-    void showscores(bool on) { UI::holdui("scoreboard", on); }
+    void showscores(bool on)
+    {
+        UI::holdui("scoreboard", on);
+    }
 }
 
