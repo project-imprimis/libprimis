project-imprimis/libprimis

commit 68689d684020d354e69950c460415fdcc3fe5b28

remove underscores from throttle* vars, closes #60

src/engine/client.cpp
@@ -32,9 +32,9 @@ VARF(rate, 0, 0, 1024, setrate(rate));
 
 void throttle();
 
-VARF(throttle_interval, 0, 5, 30, throttle());
-VARF(throttle_accel,    0, 2, 32, throttle());
-VARF(throttle_decel,    0, 2, 32, throttle());
+VARF(throttleinterval, 0, 5, 30, throttle());
+VARF(throttleaccel,    0, 2, 32, throttle());
+VARF(throttledecel,    0, 2, 32, throttle());
 
 void throttle()
 {

commit 8b92fee36808198de59073561251d369d5656735

fix var usages broken in 68689d6

src/engine/client.cpp
@@ -43,7 +43,7 @@ void throttle()
         return;
     }
     ASSERT(ENET_PEER_PACKET_THROTTLE_SCALE==32);
-    enet_peer_throttle_configure(curpeer, throttle_interval*1000, throttle_accel, throttle_decel);
+    enet_peer_throttle_configure(curpeer, throttleinterval*1000, throttleaccel, throttledecel);
 }
 
 bool isconnected(bool attempt, bool local)