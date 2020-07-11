277b0d329133d7118e5ee4385b67981b3769a3e9
Alex Foster <123afoster@gmail.com>
2020-07-10 21:03:58 -0700

    move server:: fxns out of igame.h for #126

--- src/game/game.h
+++ src/game/game.h
@@ -974,6 +974,33 @@ namespace server
     extern int msgsizelookup(int msg);
     extern bool serveroption(const char *arg);
     extern bool delayspawn(int type);
+
+    extern void *newclientinfo();
+    extern void deleteclientinfo(void *ci);
+    extern void serverinit();
+    extern int reserveclients();
+    extern int numchannels();
+    extern void clientdisconnect(int n);
+    extern int clientconnect(int n, uint ip);
+    extern void localdisconnect(int n);
+    extern void localconnect(int n);
+    extern bool allowbroadcast(int n);
+    extern void recordpacket(int chan, void *data, int len);
+    extern void parsepacket(int sender, int chan, packetbuf &p);
+    extern void sendservmsg(const char *s);
+    extern bool sendpackets(bool force = false);
+    extern void serverinforeply(ucharbuf &req, ucharbuf &p);
+    extern void serverupdate();
+    extern int protocolversion();
+    extern int laninfoport();
+    extern int serverport();
+    extern const char *defaultmaster();
+    extern int masterport();
+    extern void processmasterinput(const char *cmd, int cmdlen, const char *args);
+    extern void masterconnected();
+    extern void masterdisconnected();
+    extern bool ispaused();
+    extern int scaletime(int t);
 }
 
 #endif
--- src/shared/igame.h
+++ src/shared/igame.h
@@ -72,35 +72,6 @@ namespace game
     extern bool needminimap();
 }
 
-namespace server
-{
-    extern void *newclientinfo();
-    extern void deleteclientinfo(void *ci);
-    extern void serverinit();
-    extern int reserveclients();
-    extern int numchannels();
-    extern void clientdisconnect(int n);
-    extern int clientconnect(int n, uint ip);
-    extern void localdisconnect(int n);
-    extern void localconnect(int n);
-    extern bool allowbroadcast(int n);
-    extern void recordpacket(int chan, void *data, int len);
-    extern void parsepacket(int sender, int chan, packetbuf &p);
-    extern void sendservmsg(const char *s);
-    extern bool sendpackets(bool force = false);
-    extern void serverinforeply(ucharbuf &req, ucharbuf &p);
-    extern void serverupdate();
-    extern int protocolversion();
-    extern int laninfoport();
-    extern int serverport();
-    extern const char *defaultmaster();
-    extern int masterport();
-    extern void processmasterinput(const char *cmd, int cmdlen, const char *args);
-    extern void masterconnected();
-    extern void masterdisconnected();
-    extern bool ispaused();
-    extern int scaletime(int t);
-}
 
 extern bool isconnected(bool attempt = false, bool local = true);
 extern bool multiplayer(bool msg = true);
