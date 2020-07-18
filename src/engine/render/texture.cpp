5f24c730e7d34b77cf228795153bcf0cf0518453
Alex Foster <123afoster@gmail.com>
2020-07-17 22:28:13 -0700

    remove tga/bmp support, closes #133

--- src/engine/render/texture.cpp
+++ src/engine/render/texture.cpp
@@ -4586,187 +4586,12 @@ error:
     conoutf(Console_Error, "failed saving png to %s", filename);
 }
 
-struct tgaheader
-{
-    uchar  identsize;
-    uchar  cmaptype;
-    uchar  imagetype;
-    uchar  cmaporigin[2];
-    uchar  cmapsize[2];
-    uchar  cmapentrysize;
-    uchar  xorigin[2];
-    uchar  yorigin[2];
-    uchar  width[2];
-    uchar  height[2];
-    uchar  pixelsize;
-    uchar  descbyte;
-};
-
-VARP(compresstga, 0, 1, 1);
-
-void savetga(const char *filename, ImageData &image, bool flip)
-{
-    switch(image.bpp)
-    {
-        case 3:
-        case 4:
-        {
-            break;
-        }
-        default:
-        {
-            conoutf(Console_Error, "failed saving tga to %s", filename);
-            return;
-        }
-    }
-
-    stream *f = openfile(filename, "wb");
-    if(!f)
-    {
-        conoutf(Console_Error, "could not write to %s", filename);
-        return;
-    }
-
-    tgaheader hdr;
-    memset(&hdr, 0, sizeof(hdr));
-    hdr.pixelsize = image.bpp*8;
-    hdr.width[0] = image.w&0xFF;
-    hdr.width[1] = (image.w>>8)&0xFF;
-    hdr.height[0] = image.h&0xFF;
-    hdr.height[1] = (image.h>>8)&0xFF;
-    hdr.imagetype = compresstga ? 10 : 2;
-    f->write(&hdr, sizeof(hdr));
-
-    uchar buf[128*4];
-    for(int i = 0; i < image.h; ++i)
-    {
-        uchar *src = image.data + (flip ? i : image.h - i - 1)*image.pitch;
-        for(int remaining = image.w; remaining > 0;)
-        {
-            int raw = 1;
-            if(compresstga)
-            {
-                int run = 1;
-                for(uchar *scan = src; run < min(remaining, 128); run++)
-                {
-                    scan += image.bpp;
-                    if(src[0]!=scan[0] || src[1]!=scan[1] || src[2]!=scan[2] || (image.bpp==4 && src[3]!=scan[3]))
-                    {
-                        break;
-                    }
-                }
-                if(run > 1)
-                {
-                    f->putchar(0x80 | (run-1));
-                    f->putchar(src[2]); f->putchar(src[1]); f->putchar(src[0]);
-                    if(image.bpp==4)
-                    {
-                        f->putchar(src[3]);
-                    }
-                    src += run*image.bpp;
-                    remaining -= run;
-                    if(remaining <= 0)
-                    {
-                        break;
-                    }
-                }
-                for(uchar *scan = src; raw < min(remaining, 128); raw++)
-                {
-                    scan += image.bpp;
-                    if(src[0]==scan[0] && src[1]==scan[1] && src[2]==scan[2] && (image.bpp!=4 || src[3]==scan[3]))
-                    {
-                        break;
-                    }
-                }
-                f->putchar(raw - 1);
-            }
-            else
-            {
-                raw = min(remaining, 128);
-            }
-            uchar *dst = buf;
-            for(int j = 0; j < raw; ++j)
-            {
-                dst[0] = src[2];
-                dst[1] = src[1];
-                dst[2] = src[0];
-                if(image.bpp==4)
-                {
-                    dst[3] = src[3];
-                }
-                dst += image.bpp;
-                src += image.bpp;
-            }
-            f->write(buf, raw*image.bpp);
-            remaining -= raw;
-        }
-    }
-
-    delete f;
-}
-
-enum
-{
-    IMG_BMP = 0,
-    IMG_TGA = 1,
-    IMG_PNG = 2,
-    NUMIMG
-};
-
-VARP(screenshotformat, 0, IMG_PNG, NUMIMG-1);
-
-const char *imageexts[NUMIMG] = { ".bmp", ".tga", ".png" };
-
-int guessimageformat(const char *filename, int format = IMG_BMP)
-{
-    int len = strlen(filename);
-    for(int i = 0; i < NUMIMG; ++i)
-    {
-        int extlen = strlen(imageexts[i]);
-        if(len >= extlen && !strcasecmp(&filename[len-extlen], imageexts[i]))
-        {
-            return i;
-        }
-    }
-    return format;
-}
-
-void saveimage(const char *filename, int format, ImageData &image, bool flip = false)
-{
-    switch(format)
-    {
-        case IMG_PNG: savepng(filename, image, flip); break;
-        case IMG_TGA: savetga(filename, image, flip); break;
-        default:
-        {
-            ImageData flipped(image.w, image.h, image.bpp, image.data);
-            if(flip)
-            {
-                texflip(flipped);
-            }
-            SDL_Surface *s = wrapsurface(flipped.data, flipped.w, flipped.h, flipped.bpp);
-            if(!s)
-            {
-                break;
-            }
-            stream *f = openfile(filename, "wb");
-            if(f)
-            {
-                SDL_SaveBMP_RW(s, f->rwops(), 1);
-                delete f;
-            }
-            SDL_FreeSurface(s);
-            break;
-        }
-    }
-}
-
 SVARP(screenshotdir, "screenshot");
 
 void screenshot(char *filename)
 {
     static string buf;
-    int format = -1, dirlen = 0;
+    int dirlen = 0;
     copystring(buf, screenshotdir);
     if(screenshotdir[0])
     {
@@ -4785,27 +4610,27 @@ void screenshot(char *filename)
     if(filename[0])
     {
         concatstring(buf, filename);
-        format = guessimageformat(buf, -1);
     }
     else
     {
         string sstime;
         time_t t = time(NULL);
-        size_t len = strftime(sstime, sizeof(sstime), "%Y-%m-%d_%H.%M.%S", localtime(&t));
+        size_t len = strftime(sstime, sizeof(sstime), "%Y-%m-%d_%H.%M.%S.png", localtime(&t));
         sstime[min(len, sizeof(sstime)-1)] = '\0';
         concatstring(buf, sstime);
-        for(char *s = &buf[dirlen]; *s; s++) if(iscubespace(*s) || *s == '/' || *s == '\\') *s = '-';
-    }
-    if(format < 0)
-    {
-        format = screenshotformat;
-        concatstring(buf, imageexts[format]);
+        for(char *s = &buf[dirlen]; *s; s++)
+        {
+            if(iscubespace(*s) || *s == '/' || *s == '\\')
+            {
+                *s = '-';
+            }
+        }
     }
 
     ImageData image(screenw, screenh, 3);
     glPixelStorei(GL_PACK_ALIGNMENT, texalign(image.data, screenw, 3));
     glReadPixels(0, 0, screenw, screenh, GL_RGB, GL_UNSIGNED_BYTE, image.data);
-    saveimage(path(buf), format, image, true);
+    savepng(path(buf), image, true);
 }
 
 COMMAND(screenshot, "s");
