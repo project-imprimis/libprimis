2dbad971705512b6527f19c15688d6a7459ad2de
Alex Foster <123afoster@gmail.com>
2020-09-13 23:02:44 -0600

    tools.h quicksort expansion

--- src/shared/tools.h
+++ src/shared/tools.h
@@ -483,21 +483,60 @@ inline void quicksort(T *start, T *end, F fun)
 {
     while(end-start > 10)
     {
-        T *mid = &start[(end-start)/2], *i = start+1, *j = end-2, pivot;
+        T *mid = &start[(end-start)/2], *i = start+1,
+          *j = end-2,
+          pivot;
         if(fun(*start, *mid)) /* start < mid */
         {
-            if(fun(end[-1], *start)) { pivot = *start; *start = end[-1]; end[-1] = *mid; } /* end < start < mid */
-            else if(fun(end[-1], *mid)) { pivot = end[-1]; end[-1] = *mid; } /* start <= end < mid */
-            else { pivot = *mid; } /* start < mid <= end */
+            if(fun(end[-1], *start))
+            {
+                pivot = *start;
+                *start = end[-1];
+                end[-1] = *mid;
+            } /* end < start < mid */
+            else if(fun(end[-1], *mid))
+            {
+                pivot = end[-1];
+                end[-1] = *mid;
+            } /* start <= end < mid */
+            else
+            {
+                pivot = *mid;
+            } /* start < mid <= end */
         }
-        else if(fun(*start, end[-1])) { pivot = *start; *start = *mid; } /*mid <= start < end */
-        else if(fun(*mid, end[-1])) { pivot = end[-1]; end[-1] = *start; *start = *mid; } /* mid < end <= start */
-        else { pivot = *mid; swap(*start, end[-1]); }  /* end <= mid <= start */
+        else if(fun(*start, end[-1]))
+        {
+            pivot = *start;
+            *start = *mid;
+        } /*mid <= start < end */
+        else if(fun(*mid, end[-1]))
+        {
+            pivot = end[-1];
+            end[-1] = *start;
+            *start = *mid;
+        } /* mid < end <= start */
+        else
+        {
+            pivot = *mid;
+            swap(*start, end[-1]);
+        }  /* end <= mid <= start */
         *mid = end[-2];
         do
         {
-            while(fun(*i, pivot)) if(++i >= j) goto partitioned;
-            while(fun(pivot, *--j)) if(i >= j) goto partitioned;
+            while(fun(*i, pivot))
+            {
+                if(++i >= j)
+                {
+                    goto partitioned;
+                }
+            }
+            while(fun(pivot, *--j))
+            {
+                if(i >= j)
+                {
+                    goto partitioned;
+                }
+            }
             swap(*i, *j);
         } while(++i < j);
     partitioned:
