73398f5d5e1e668dbcf68bef318c05e31cc70743
Alex Foster <123afoster@gmail.com>
2020-10-15 22:01:21 -0700

    move engine timer functionality to rendertimers.cpp
    timer code was part of rendergl but is easily separable and distinct

---
+++ src/engine/render/rendertimers.cpp
@@ -0,0 +1,177 @@
+#include "engine.h"
+/* rendertimers.cpp: renderer functionality used for displaying rendering stats
+ * while the program is running
+ * 
+ * timers can be created with designated start/stop points in the code; sub-ms
+ * times needed for accurate diagnosis possible (each frame is ~16.6ms @ 60Hz)
+ * 
+ * used in rendergl.cpp
+ */
+ 
+struct timer
+{
+    enum
+    {
+        MAXQUERY = 4
+    };
+    const char *name;
+    bool gpu;
+    GLuint query[MAXQUERY];
+    int waiting;
+    uint starttime;
+    float result, print;
+};
+static vector<timer> timers;
+static vector<int> timerorder;
+static int timercycle = 0;
+
+extern int usetimers;
+
+timer *findtimer(const char *name, bool gpu)
+{
+    for(int i = 0; i < timers.length(); i++)
+    {
+        if(!strcmp(timers[i].name, name) && timers[i].gpu == gpu)
+        {
+            timerorder.removeobj(i);
+            timerorder.add(i);
+            return &timers[i];
+        }
+    }
+    timerorder.add(timers.length());
+    timer &t = timers.add();
+    t.name = name;
+    t.gpu = gpu;
+    memset(t.query, 0, sizeof(t.query));
+    if(gpu)
+    {
+        glGenQueries_(timer::MAXQUERY, t.query);
+    }
+    t.waiting = 0;
+    t.starttime = 0;
+    t.result = -1;
+    t.print = -1;
+    return &t;
+}
+
+timer *begintimer(const char *name, bool gpu)
+{
+    if(!usetimers || inbetweenframes || (gpu && (!hasTQ || deferquery)))
+    {
+        return NULL;
+    }
+    timer *t = findtimer(name, gpu);
+    if(t->gpu)
+    {
+        deferquery++;
+        glBeginQuery_(GL_TIME_ELAPSED_EXT, t->query[timercycle]);
+        t->waiting |= 1<<timercycle;
+    }
+    else
+    {
+        t->starttime = getclockmillis();
+    }
+    return t;
+}
+
+void endtimer(timer *t)
+{
+    if(!t)
+    {
+        return;
+    }
+    if(t->gpu)
+    {
+        glEndQuery_(GL_TIME_ELAPSED_EXT);
+        deferquery--;
+    }
+    else
+    {
+        t->result = max(static_cast<float>(getclockmillis() - t->starttime), 0.0f);
+    }
+}
+
+void synctimers()
+{
+    timercycle = (timercycle + 1) % timer::MAXQUERY;
+
+    for(int i = 0; i < timers.length(); i++)
+    {
+        timer &t = timers[i];
+        if(t.waiting&(1<<timercycle))
+        {
+            GLint available = 0;
+            while(!available)
+            {
+                glGetQueryObjectiv_(t.query[timercycle], GL_QUERY_RESULT_AVAILABLE, &available);
+            }
+            GLuint64EXT result = 0;
+            glGetQueryObjectui64v_(t.query[timercycle], GL_QUERY_RESULT, &result);
+            t.result = max(static_cast<float>(result) * 1e-6f, 0.0f);
+            t.waiting &= ~(1<<timercycle);
+        }
+        else
+        {
+            t.result = -1;
+        }
+    }
+}
+
+void cleanuptimers()
+{
+    for(int i = 0; i < timers.length(); i++)
+    {
+        timer &t = timers[i];
+        if(t.gpu)
+        {
+            glDeleteQueries_(timer::MAXQUERY, t.query);
+        }
+    }
+    timers.shrink(0);
+    timerorder.shrink(0);
+}
+
+VARFN(timer, usetimers, 0, 0, 1, cleanuptimers());
+VAR(frametimer, 0, 0, 1);
+int framemillis = 0; // frame time (ie does not take into account the swap)
+
+void printtimers(int conw, int conh)
+{
+    if(!frametimer && !usetimers)
+    {
+        return;
+    }
+    static int lastprint = 0;
+    int offset = 0;
+    if(frametimer)
+    {
+        static int printmillis = 0;
+        if(totalmillis - lastprint >= 200)
+        {
+            printmillis = framemillis;
+        }
+        draw_textf("frame time %i ms", conw-20*FONTH, conh-FONTH*3/2-offset*9*FONTH/8, printmillis);
+        offset++;
+    }
+    if(usetimers)
+    {
+        for(int i = 0; i < timerorder.length(); i++)
+        {
+            timer &t = timers[timerorder[i]];
+            if(t.print < 0 ? t.result >= 0 : totalmillis - lastprint >= 200)
+            {
+                t.print = t.result;
+            }
+            if(t.print < 0 || (t.gpu && !(t.waiting&(1<<timercycle))))
+            {
+                continue;
+            }
+            draw_textf("%s%s %5.2f ms", conw-20*FONTH, conh-FONTH*3/2-offset*9*FONTH/8, t.name, t.gpu ? "" : " (cpu)", t.print);
+            offset++;
+        }
+    }
+    if(totalmillis - lastprint >= 200)
+    {
+        lastprint = totalmillis;
+    }
+}
---
+++ src/engine/render/rendertimers.h
@@ -0,0 +1,10 @@
+#ifndef RENDERTIMERS_H_
+#define RENDERTIMERS_H_
+
+extern void printtimers(int conw, int conh);
+extern int frametimer;
+extern int framemillis;
+extern void synctimers();
+extern void cleanuptimers();
+
+#endif
