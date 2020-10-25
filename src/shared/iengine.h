dc5ba4fe0f5962ed939b931dd5dc8fbd49ca35cb
Alex Foster <123afoster@gmail.com>
2020-10-24 23:52:46 -0700

    remove unused server crypto fxns for #159

--- src/shared/iengine.h
+++ src/shared/iengine.h
@@ -50,12 +50,6 @@ extern void genprivkey(const char *seed, vector<char> &privstr, vector<char> &pu
 extern bool calcpubkey(const char *privstr, vector<char> &pubstr);
 extern bool hashstring(const char *str, char *result, int maxlen);
 extern void answerchallenge(const char *privstr, const char *challenge, vector<char> &answerstr);
-extern void *parsepubkey(const char *pubstr);
-extern void freepubkey(void *pubkey);
-extern void *genchallenge(void *pubkey, const void *seed, int seedlen, vector<char> &challengestr);
-extern void freechallenge(void *answer);
-extern bool checkchallenge(const char *answerstr, void *correct);
-
 
 /*==============================================================================*\
  * Interface Functions & Values                                                 *
