7e917420efa746ef061eba52ffe7fb79f0e8be8c
Alex Foster <123afoster@gmail.com>
2021-03-10 23:56:12 -0800

    bih.h std::abs for #52

--- src/engine/world/bih.h
+++ src/engine/world/bih.h
@@ -22,9 +22,9 @@ struct BIH
         svec center, radius;
         bool outside(const ivec &bo, const ivec &br) const
         {
-            return abs(bo.x - center.x) > br.x + radius.x ||
-                   abs(bo.y - center.y) > br.y + radius.y ||
-                   abs(bo.z - center.z) > br.z + radius.z;
+            return std::abs(bo.x - center.x) > br.x + radius.x ||
+                   std::abs(bo.y - center.y) > br.y + radius.y ||
+                   std::abs(bo.z - center.z) > br.z + radius.z;
         }
     };
 
