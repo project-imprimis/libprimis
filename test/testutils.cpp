98063332e485497c4328e7c12f0e62d965e694bb
Alex Foster <123afoster@gmail.com>
2021-06-27 23:55:52 -0700

    namespace & expand vector test coverage

--- test/testutils.cpp
+++ test/testutils.cpp
@@ -1,6 +1,35 @@
 #include "libprimis.h"
 
-namespace
+namespace header_tools
+{
+    void testvector()
+    {
+        vector<int> a;
+        assert(a.length() == 0);
+        assert(a.empty());
+        //check adding and removing a member
+        a.add(1);
+        assert(a.length() == 1);
+        a.remove(0);
+        assert(a.length() == 0);
+
+        //check adding a large member (vector.put())
+        const char * testtext = "large text",
+                   * testtextstart = "l",
+                   * testtextend   = "t";
+
+        vector<char> b;
+        b.put(testtext, 10);
+        assert(b.length() == 10);
+        assert(b[0] == *testtextstart);
+        assert(b[9] == *testtextend);
+
+        //setsize
+        b.setsize(5);
+        assert(b.length() == 5);
+    }
+}
+namespace header_geom
 {
     void testgenericvec3()
     {
@@ -9,6 +38,12 @@ namespace
         GenericVec3<float> c = a+b;
 
         c = a-b;
+
+        bool d = a > b;
+        d = a < b;
+        d = a >= b;
+        d = a <= b;
+        d = a == b;
         assert(c == vec(0,0,0));
     }
 
@@ -20,7 +55,7 @@ namespace
         a.mul(vec(2,2,3));
         a.add(vec(1,1,1));
         a.sub(vec(1,1,1));
-        a.mul(b);
+        a.mul(2);
     }
 
     void testmod360()
@@ -37,13 +72,16 @@ namespace
     void testcrc()
     {
         clearmapcrc();
+        entitiesinoctanodes();
+        getworldsize();
     }
 }
 
 void testutils()
 {
-    testgenericvec3();
-    testvec3();
-    testmod360();
-    testcrc();
+    header_tools::testvector();
+    header_geom::testgenericvec3();
+    header_geom::testvec3();
+    header_geom::testmod360();
+    header_geom::testcrc();
 }
