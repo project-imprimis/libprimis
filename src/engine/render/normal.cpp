4c2afd8f1c99279c284cfdebf3b48842e1914eea
Alex Foster <123afoster@gmail.com>
2021-09-15 23:15:39 +0200

    fix normal.cpp commenting

--- src/engine/render/normal.cpp
+++ src/engine/render/normal.cpp
@@ -409,7 +409,7 @@ void resetsmoothgroups()
 }
 
 static constexpr int maxsmoothgroups = 10000;
-//smooths the smoothgroup at the idth location in the smoothgroups vector
+//returns the smoothgroup at the idth location in the smoothgroups vector
 //returns -1 (failure) if you try to ask for an id greater than 10,000
 int smoothangle(int id, int angle)
 {
