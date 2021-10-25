750ca2c946f3d64a040e92824390c12f513f9f4a
Alex Foster <123afoster@gmail.com>
2021-10-24 23:38:34 -0700

    move setupdata from vacollect to vtxarray

--- src/engine/render/octarender.h
+++ src/engine/render/octarender.h
@@ -23,6 +23,8 @@ struct renderstate
 
 struct decalrenderer;
 
+class vacollect;
+
 class vtxarray
 {
     public:
@@ -84,6 +86,7 @@ class vtxarray
         void changevbuf(decalrenderer &cur);
         void changevbuf(renderstate &cur, int pass);
         uchar * addvbo(int type, int numelems, int elemsize);
+        void setupdata(vacollect* vacol);
 
 
     private:
