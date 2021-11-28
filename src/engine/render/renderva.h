b45bd28b1a327c9222c69d03c05a18de59200905
Alex Foster <123afoster@gmail.com>
2021-11-27 18:44:11 -0800

    move vfc::calc/setvfcd to be private

--- src/engine/render/renderva.h
+++ src/engine/render/renderva.h
@@ -6,14 +6,15 @@ class vfc
     public:
         int isfoggedcube(const ivec &o, int size);
         int isvisiblecube(const ivec &o, int size);
-        void calcvfcD();
         void visiblecubes(bool cull = true);
         bool isfoggedsphere(float rad, const vec &cv);
         int isvisiblesphere(float rad, const vec &cv);
         int isvisiblebb(const ivec &bo, const ivec &br);
-        void setvfcP(const vec &bbmin = vec(-1, -1, -1), const vec &bbmax = vec(1, 1, 1));
         int cullfrustumsides(const vec &lightpos, float lightradius, float size, float border);
     private:
+        void calcvfcD();
+        void setvfcP(const vec &bbmin = vec(-1, -1, -1), const vec &bbmax = vec(1, 1, 1));
+
         plane vfcP[5];  // perpindictular vectors to view frustrum bounding planes
         float vfcDfog;  // far plane culling distance (fog limit).
         float vfcDnear[5], //near plane culling
