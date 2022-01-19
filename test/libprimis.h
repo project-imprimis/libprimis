a2a382ac1fe67ada4fce3f44673db3586b08d7a6
Luis David Licea Torres <luislicea1011@hotmail.com>
2022-01-18 23:50:18 -0600

    Remove texture.h header.
    The texture.h header does not exist anymore, so it needed to be removed.

--- test/libprimis.h
+++ test/libprimis.h
@@ -4,6 +4,5 @@
 #include "../src/libprimis-headers/cube.h"
 #include "../src/libprimis-headers/iengine.h"
 #include "../src/libprimis-headers/consts.h"
-#include "../src/libprimis-headers/texture.h"
 
 #endif
