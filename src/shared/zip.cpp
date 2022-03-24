89e1560211927fcbfeb85592fac50852c0b32f2d
Alex Foster <123afoster@gmail.com>
2022-03-24 21:17:00 +0100

    std::memcpy zip.cpp for #52

--- src/shared/zip.cpp
+++ src/shared/zip.cpp
@@ -185,7 +185,7 @@ static bool readzipdirectory(const char *archname, FILE *f, int entries, int off
         }
         string pname;
         int namelen = std::min(static_cast<int>(hdr.namelength), static_cast<int>(sizeof(pname)-1));
-        memcpy(pname, src, namelen);
+        std::memcpy(pname, src, namelen);
         pname[namelen] = '\0';
         path(pname);
         char *name = newstring(pname);
