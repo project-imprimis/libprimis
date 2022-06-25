9027083abcaab11b4496daa7257df5f3bafaf8b2
Alex Foster <123afoster@gmail.com>
2022-06-24 23:32:00 -0700

    rename SETSTR function to uisetstr

--- src/engine/interface/ui.cpp
+++ src/engine/interface/ui.cpp
@@ -2526,7 +2526,7 @@ namespace UI
     VARP(uitextrows, 1, 24, 200);
     FVAR(uitextscale, 1, 0, 0);
 
-    static void SETSTR(char * dst, const char * src)
+    static void uisetstr(char * dst, const char * src)
     {
         if(dst)
         {
@@ -2612,7 +2612,7 @@ namespace UI
         {
             Text::setup(scale_, color_, wrap_);
 
-            SETSTR(str, str_);
+            uisetstr(str, str_);
         }
 
         static const char *typestr()
@@ -3668,7 +3668,7 @@ namespace UI
             scale = scale_;
             if(keyfilter_)
             {
-                SETSTR(keyfilter, keyfilter_);
+                uisetstr(keyfilter, keyfilter_);
             }
             else
             {
@@ -4103,7 +4103,7 @@ namespace UI
         void setup(const char *name_, const char *animspec, float minw_, float minh_)
         {
             Preview::setup(minw_, minh_);
-            SETSTR(name, name_);
+            uisetstr(name, name_);
 
             anim = Anim_All;
             if(animspec[0])
@@ -4178,7 +4178,7 @@ namespace UI
         void setup(const char *name_, int color_, float minw_, float minh_)
         {
             Preview::setup(minw_, minh_);
-            SETSTR(name, name_);
+            uisetstr(name, name_);
             color = vec::hexcolor(color_);
         }
 
