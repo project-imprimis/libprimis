852b6adc7cbea4fee52d3a35e530b3cbcfc76400
Alex Foster <123afoster@gmail.com>
2022-07-06 21:46:19 -0700

    remove unused raycubelos()

--- src/engine/world/raycube.h
+++ src/engine/world/raycube.h
@@ -6,7 +6,6 @@ extern vec hitsurface;
 extern float raycubepos(const vec &o, const vec &ray, vec &hit, float radius = 0, int mode = Ray_ClipMat, int size = 0);
 extern float rayent(const vec &o, const vec &ray, float radius, int mode, int size, int &orient, int &ent);
 extern float rayfloor  (const vec &o, vec &floor, int mode = 0, float radius = 0);
-extern bool  raycubelos(const vec &o, const vec &dest, vec &hitpos);
 
 extern bool insideworld(const vec &o);
 extern bool insideworld(const ivec &o);
