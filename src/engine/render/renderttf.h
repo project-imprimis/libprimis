422d01508a5cdb4581d99a3ecaabb2417cf77ce5
Alex Foster <123afoster@gmail.com>
2022-09-24 23:46:48 -0700

    renderttf field comment improvements

--- src/engine/render/renderttf.h
+++ src/engine/render/renderttf.h
@@ -9,9 +9,9 @@ class TTFRenderer
         void renderttf(const char* message, SDL_Color col, int x, int y, float scale = 1.f);
         void fontsize(int pts = 12);
     private:
-        TTF_Font* f; //the current working font
+        TTF_Font* f;                         //the current working font
         std::map<int, TTF_Font *> fontcache; //different sizes of the font are cached in a map which maps them to their size in pt
-        const char * path;
+        const char * path;                   //the path which the font was originally found, so it can load other font sizes if needed
 };
 
 extern TTFRenderer ttr;
