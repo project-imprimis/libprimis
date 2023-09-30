cce13da17f4a902a19da808a6b3999607b9dd471
Alex Foster <123afoster@gmail.com>
2023-09-30 23:59:53 +0900

    string in loaddefaultparts

--- src/engine/model/obj.cpp
+++ src/engine/model/obj.cpp
@@ -288,12 +288,12 @@ void obj::objmeshgroup::flushmesh(const string meshname,
 bool obj::loaddefaultparts()
 {
     part &mdl = addpart();
-    const char *pname = parentdir(name.c_str());
+    std::string pname = parentdir(name.c_str());
     DEF_FORMAT_STRING(name1, "media/model/%s/tris.obj", name.c_str());
     mdl.meshes = sharemeshes(path(name1));
     if(!mdl.meshes)
     {
-        DEF_FORMAT_STRING(name2, "media/model/%s/tris.obj", pname);    // try obj in parent folder (vert sharing)
+        DEF_FORMAT_STRING(name2, "media/model/%s/tris.obj", pname.c_str());    // try obj in parent folder (vert sharing)
         mdl.meshes = sharemeshes(path(name2));
         if(!mdl.meshes)
         {
@@ -301,7 +301,7 @@ bool obj::loaddefaultparts()
         }
     }
     Texture *tex, *masks;
-    loadskin(name.c_str(), pname, tex, masks);
+    loadskin(name.c_str(), pname.c_str(), tex, masks);
     mdl.initskins(tex, masks);
     if(tex==notexture)
     {
