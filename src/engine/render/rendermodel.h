ea5abfa072df5584e0cc8a316d284869ca2b7824
Alex Foster <123afoster@gmail.com>
2023-12-08 22:23:18 -0800

    remove redundant findanims() declaration

--- src/engine/render/rendermodel.h
+++ src/engine/render/rendermodel.h
@@ -11,8 +11,6 @@ struct mapmodelinfo
 extern std::vector<mapmodelinfo> mapmodels;
 extern std::vector<std::string> animnames; //set by game at runtime
 
-std::vector<int> findanims(const char *pattern);
-
 extern void loadskin(const std::string &dir, const std::string &altdir, Texture *&skin, Texture *&masks);
 extern model *loadmodel(const char *name, int i = -1, bool msg = false);
 extern void resetmodelbatches();
