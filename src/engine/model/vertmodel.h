8d52f438025d018b77ef01832fee096b39f5b852
Alex Foster <123afoster@gmail.com>
2023-12-26 22:02:01 -0800

    make vertmodel::loadmeshes private

--- src/engine/model/vertmodel.h
+++ src/engine/model/vertmodel.h
@@ -1,244 +1,248 @@
-struct vertmodel : animmodel
+class vertmodel : public animmodel
 {
-    struct vert final
-    {
-        vec pos, norm;
-        vec4<float> tangent;
-    };
-
-    struct vvert final
-    {
-        vec pos;
-        GenericVec2<half> tc;
-        squat tangent;
-    };
-
-    struct vvertg final
-    {
-        vec4<half> pos;
-        GenericVec2<half> tc;
-        squat tangent;
-    };
-
-    struct tcvert final
-    {
-        vec2 tc;
-    };
-
-    struct tri final
-    {
-        uint vert[3];
-    };
-
-    struct vbocacheentry final
-    {
-        GLuint vbuf;
-        AnimState as;
-        int millis;
-
-        vbocacheentry();
-    };
-
-    struct vertmesh : Mesh
-    {
-        vert *verts;
-        tcvert *tcverts;
-        tri *tris;
-        int numverts, numtris;
-
-        int voffset, elen;
-        uint minvert, maxvert;
-
-        vertmesh();
-        virtual ~vertmesh();
-
-        void smoothnorms(float limit = 0, bool areaweight = true);
-        void buildnorms(bool areaweight = true);
-        void calctangents(bool areaweight = true);
-        void calcbb(vec &bbmin, vec &bbmax, const matrix4x3 &m) override final;
-        void genBIH(BIH::mesh &m) const override final;
-        void genshadowmesh(std::vector<triangle> &out, const matrix4x3 &m) const override final;
-
-        static void assignvert(vvertg &vv, int j, const tcvert &tc, const vert &v);
-
-        template<class T>
-        int genvbo(std::vector<uint> &idxs, int offset, std::vector<T> &vverts, int *htdata, int htlen)
+    public:
+        struct vert final
         {
-            voffset = offset;
-            minvert = UINT_MAX;
-            for(int i = 0; i < numtris; ++i)
+            vec pos, norm;
+            vec4<float> tangent;
+        };
+
+        struct vvert final
+        {
+            vec pos;
+            GenericVec2<half> tc;
+            squat tangent;
+        };
+
+        struct vvertg final
+        {
+            vec4<half> pos;
+            GenericVec2<half> tc;
+            squat tangent;
+        };
+
+        struct tcvert final
+        {
+            vec2 tc;
+        };
+
+        struct tri final
+        {
+            uint vert[3];
+        };
+
+        struct vbocacheentry final
+        {
+            GLuint vbuf;
+            AnimState as;
+            int millis;
+
+            vbocacheentry();
+        };
+
+        struct vertmesh : Mesh
+        {
+            vert *verts;
+            tcvert *tcverts;
+            tri *tris;
+            int numverts, numtris;
+
+            int voffset, elen;
+            uint minvert, maxvert;
+
+            vertmesh();
+            virtual ~vertmesh();
+
+            void smoothnorms(float limit = 0, bool areaweight = true);
+            void buildnorms(bool areaweight = true);
+            void calctangents(bool areaweight = true);
+            void calcbb(vec &bbmin, vec &bbmax, const matrix4x3 &m) override final;
+            void genBIH(BIH::mesh &m) const override final;
+            void genshadowmesh(std::vector<triangle> &out, const matrix4x3 &m) const override final;
+
+            static void assignvert(vvertg &vv, int j, const tcvert &tc, const vert &v);
+
+            template<class T>
+            int genvbo(std::vector<uint> &idxs, int offset, std::vector<T> &vverts, int *htdata, int htlen)
             {
-                tri &t = tris[i];
-                for(int j = 0; j < 3; ++j)
+                voffset = offset;
+                minvert = UINT_MAX;
+                for(int i = 0; i < numtris; ++i)
                 {
-                    int index = t.vert[j];
-                    vert &v = verts[index];
-                    tcvert &tc = tcverts[index];
-                    T vv;
-                    assignvert(vv, index, tc, v);
-                    auto hashfn = std::hash<vec>();
-                    int htidx = hashfn(v.pos)&(htlen-1);
-                    for(int k = 0; k < htlen; ++k)
+                    tri &t = tris[i];
+                    for(int j = 0; j < 3; ++j)
                     {
-                        int &vidx = htdata[(htidx+k)&(htlen-1)];
-                        if(vidx < 0)
+                        int index = t.vert[j];
+                        vert &v = verts[index];
+                        tcvert &tc = tcverts[index];
+                        T vv;
+                        assignvert(vv, index, tc, v);
+                        auto hashfn = std::hash<vec>();
+                        int htidx = hashfn(v.pos)&(htlen-1);
+                        for(int k = 0; k < htlen; ++k)
                         {
-                            idxs.push_back(vverts.size());
-                            vidx = idxs.back();
-                            vverts.push_back(vv);
-                            break;
-                        }
-                        else if(!std::memcmp(&vverts[vidx], &vv, sizeof(vv)))
-                        {
-                            idxs.push_back(static_cast<uint>(vidx));
-                            minvert = std::min(minvert, idxs.back());
-                            break;
+                            int &vidx = htdata[(htidx+k)&(htlen-1)];
+                            if(vidx < 0)
+                            {
+                                idxs.push_back(vverts.size());
+                                vidx = idxs.back();
+                                vverts.push_back(vv);
+                                break;
+                            }
+                            else if(!std::memcmp(&vverts[vidx], &vv, sizeof(vv)))
+                            {
+                                idxs.push_back(static_cast<uint>(vidx));
+                                minvert = std::min(minvert, idxs.back());
+                                break;
+                            }
                         }
                     }
                 }
+                minvert = std::min(minvert, static_cast<uint>(voffset));
+                maxvert = std::max(minvert, static_cast<uint>(vverts.size()-1));
+                elen = idxs.size();
+                return vverts.size()-voffset;
             }
-            minvert = std::min(minvert, static_cast<uint>(voffset));
-            maxvert = std::max(minvert, static_cast<uint>(vverts.size()-1));
-            elen = idxs.size();
-            return vverts.size()-voffset;
-        }
 
-        int genvbo(std::vector<uint> &idxs, int offset);
+            int genvbo(std::vector<uint> &idxs, int offset);
 
-        template<class T>
-        static void fillvert(T &vv, tcvert &tc, vert &v)
-        {
-            vv.tc = tc.tc;
-        }
-
-        template<class T>
-        void fillverts(T *vdata)
-        {
-            vdata += voffset;
-            for(int i = 0; i < numverts; ++i)
+            template<class T>
+            static void fillvert(T &vv, tcvert &tc, vert &v)
             {
-                fillvert(vdata[i], tcverts[i], verts[i]);
+                vv.tc = tc.tc;
             }
-        }
 
-        template<class T>
-        void interpverts(const AnimState &as, T * RESTRICT vdata)
-        {
-            vdata += voffset;
-            const vert * RESTRICT vert1 = &verts[as.cur.fr1 * numverts],
-                       * RESTRICT vert2 = &verts[as.cur.fr2 * numverts],
-                       * RESTRICT pvert1 = as.interp<1 ? &verts[as.prev.fr1 * numverts] : nullptr,
-                       * RESTRICT pvert2 = as.interp<1 ? &verts[as.prev.fr2 * numverts] : nullptr;
-            if(as.interp<1)
+            template<class T>
+            void fillverts(T *vdata)
             {
+                vdata += voffset;
                 for(int i = 0; i < numverts; ++i)
                 {
-                    T &v = vdata[i];
-                    v.pos.lerp(vec().lerp(pvert1[i].pos, pvert2[i].pos, as.prev.t),
-                               vec().lerp(vert1[i].pos, vert2[i].pos, as.cur.t),
-                               as.interp);
-                    v.tangent.lerp(vec4<float>().lerp(pvert1[i].tangent, pvert2[i].tangent, as.prev.t),
-                                   vec4<float>().lerp(vert1[i].tangent, vert2[i].tangent, as.cur.t),
+                    fillvert(vdata[i], tcverts[i], verts[i]);
+                }
+            }
+
+            template<class T>
+            void interpverts(const AnimState &as, T * RESTRICT vdata)
+            {
+                vdata += voffset;
+                const vert * RESTRICT vert1 = &verts[as.cur.fr1 * numverts],
+                           * RESTRICT vert2 = &verts[as.cur.fr2 * numverts],
+                           * RESTRICT pvert1 = as.interp<1 ? &verts[as.prev.fr1 * numverts] : nullptr,
+                           * RESTRICT pvert2 = as.interp<1 ? &verts[as.prev.fr2 * numverts] : nullptr;
+                if(as.interp<1)
+                {
+                    for(int i = 0; i < numverts; ++i)
+                    {
+                        T &v = vdata[i];
+                        v.pos.lerp(vec().lerp(pvert1[i].pos, pvert2[i].pos, as.prev.t),
+                                   vec().lerp(vert1[i].pos, vert2[i].pos, as.cur.t),
                                    as.interp);
+                        v.tangent.lerp(vec4<float>().lerp(pvert1[i].tangent, pvert2[i].tangent, as.prev.t),
+                                       vec4<float>().lerp(vert1[i].tangent, vert2[i].tangent, as.cur.t),
+                                       as.interp);
+                    }
+                }
+                else
+                {
+                    for(int i = 0; i < numverts; ++i)
+                    {
+                        T &v = vdata[i];
+                        v.pos.lerp(vert1[i].pos, vert2[i].pos, as.cur.t);
+                        v.tangent.lerp(vert1[i].tangent, vert2[i].tangent, as.cur.t);
+                    }
                 }
             }
-            else
+
+            void render();
+        };
+
+        struct tag final
+        {
+            char *name;
+            matrix4x3 matrix;
+
+            tag() : name(nullptr) {}
+            ~tag()
             {
-                for(int i = 0; i < numverts; ++i)
-                {
-                    T &v = vdata[i];
-                    v.pos.lerp(vert1[i].pos, vert2[i].pos, as.cur.t);
-                    v.tangent.lerp(vert1[i].tangent, vert2[i].tangent, as.cur.t);
-                }
+                delete[] name;
             }
-        }
+        };
 
-        void render();
-    };
-
-    struct tag final
-    {
-        char *name;
-        matrix4x3 matrix;
-
-        tag() : name(nullptr) {}
-        ~tag()
+        struct vertmeshgroup : meshgroup
         {
-            delete[] name;
-        }
-    };
+            int numframes;
+            tag *tags;
+            int numtags;
 
-    struct vertmeshgroup : meshgroup
-    {
-        int numframes;
-        tag *tags;
-        int numtags;
+            static constexpr int maxvbocache = 16;
+            vbocacheentry vbocache[maxvbocache];
 
-        static constexpr int maxvbocache = 16;
-        vbocacheentry vbocache[maxvbocache];
+            GLuint ebuf;
+            int vlen, vertsize;
+            uchar *vdata;
 
-        GLuint ebuf;
-        int vlen, vertsize;
-        uchar *vdata;
+            vertmeshgroup();
+            virtual ~vertmeshgroup();
 
-        vertmeshgroup();
-        virtual ~vertmeshgroup();
+            virtual void concattagtransform(int i, const matrix4x3 &m, matrix4x3 &n) const override final;
+            bool addtag(const char *name, const matrix4x3 &matrix);
+            int findtag(const char *name) override final;
 
-        virtual void concattagtransform(int i, const matrix4x3 &m, matrix4x3 &n) const override final;
-        bool addtag(const char *name, const matrix4x3 &matrix);
-        int findtag(const char *name) override final;
+            int totalframes() const override final;
+            void calctagmatrix(const part *p, int i, const AnimState &as, matrix4 &matrix) const;
 
-        int totalframes() const override final;
-        void calctagmatrix(const part *p, int i, const AnimState &as, matrix4 &matrix) const;
+            void genvbo(vbocacheentry &vc);
 
-        void genvbo(vbocacheentry &vc);
-
-        template<class T>
-        void bindvbo(const AnimState *as, const part *p, const vbocacheentry &vc)
-        {
-            T *vverts = 0;
-            bindpos(ebuf, vc.vbuf, &vverts->pos, vertsize);
-            if(as->cur.anim & Anim_NoSkin)
+            template<class T>
+            void bindvbo(const AnimState *as, const part *p, const vbocacheentry &vc)
             {
-                if(enabletangents)
+                T *vverts = 0;
+                bindpos(ebuf, vc.vbuf, &vverts->pos, vertsize);
+                if(as->cur.anim & Anim_NoSkin)
                 {
-                    disabletangents();
+                    if(enabletangents)
+                    {
+                        disabletangents();
+                    }
+                    if(p->alphatested())
+                    {
+                        bindtc(&vverts->tc, vertsize);
+                    }
+                    else if(enabletc)
+                    {
+                        disabletc();
+                    }
                 }
-                if(p->alphatested())
+                else
                 {
+                    bindtangents(&vverts->tangent, vertsize);
                     bindtc(&vverts->tc, vertsize);
                 }
-                else if(enabletc)
+                if(enablebones)
                 {
-                    disabletc();
+                    disablebones();
                 }
             }
-            else
-            {
-                bindtangents(&vverts->tangent, vertsize);
-                bindtc(&vverts->tc, vertsize);
-            }
-            if(enablebones)
-            {
-                disablebones();
-            }
-        }
 
-        void bindvbo(const AnimState *as, const part *p, const vbocacheentry &vc);
-        void *animkey() override final;
-        void cleanup() override final;
-        void preload() override final;
-        void render(const AnimState *as, float, const vec &, const vec &, dynent *, part *p) override final;
+            void bindvbo(const AnimState *as, const part *p, const vbocacheentry &vc);
+            void *animkey() override final;
+            void cleanup() override final;
+            void preload() override final;
+            void render(const AnimState *as, float, const vec &, const vec &, dynent *, part *p) override final;
 
-        virtual bool load(const char *name, float smooth) = 0;
-    };
+            virtual bool load(const char *name, float smooth) = 0;
+        };
 
-    virtual vertmeshgroup *newmeshes() = 0;
+        virtual vertmeshgroup *newmeshes() = 0;
 
-    meshgroup *loadmeshes(const char *name, float smooth = 2);
-    meshgroup *sharemeshes(const char *name, float smooth = 2);
+        meshgroup *sharemeshes(const char *name, float smooth = 2);
 
-    vertmodel(std::string name);
+        vertmodel(std::string name);
+
+    private:
+        meshgroup *loadmeshes(const char *name, float smooth = 2);
+
 };
 
 template<class MDL>
