45ea0f3e20a2b5b28485d1e0d0e7682d38cc9f03
Alex Foster <123afoster@gmail.com>
2024-02-16 23:06:07 -0800

    add matrix3 ctor tests

--- test/testmatrix.cpp
+++ test/testmatrix.cpp
@@ -1,13 +1,83 @@
 
 #include "libprimis.h"
+#include "../src/shared/geomexts.h"
+
+constexpr float tolerance = 0.001;
 
 void test_matrix3_ctor()
 {
     std::printf("testing matrix3 constructor\n");
-    matrix3 m;
-    assert(m.a == vec(0,0,0));
-    assert(m.b == vec(0,0,0));
-    assert(m.c == vec(0,0,0));
+    //matrix3()
+    {
+        matrix3 m;
+        assert(m.a == vec(0,0,0));
+        assert(m.b == vec(0,0,0));
+        assert(m.c == vec(0,0,0));
+    }
+    //matrix3(vec,vec,vec)
+    {
+        vec a(1,2,3),
+            b(4,5,6),
+            c(7,8,9);
+        matrix3 m(a,b,c);
+        assert(m.a == a);
+        assert(m.b == b);
+        assert(m.c == c);
+    }
+    //matrix3(quat)
+    {
+        //test use of normalized quat -> normalized matrix
+        quat q(0.5,0.5,0.5,0.5);
+        matrix3 m(q);
+        assert(m.a.sub(vec(0,1,0)).magnitude() < tolerance);
+        assert(m.b.sub(vec(0,0,1)).magnitude() < tolerance);
+        assert(m.c.sub(vec(1,0,0)).magnitude() < tolerance);
+    }
+    {
+        //test identity quat -> identity matrix
+        quat q(0,0,0,1);
+        matrix3 m(q);
+        assert(m.a.sub(vec(1,0,0)).magnitude() < tolerance);
+        assert(m.b.sub(vec(0,1,0)).magnitude() < tolerance);
+        assert(m.c.sub(vec(0,0,1)).magnitude() < tolerance);
+    }
+    //matrix3(float,vec)
+    {
+        //test identity generation
+        vec axis(1,0,0);
+        float angle = 0;
+        matrix3 m(angle, axis);
+        assert(m.a.sub(vec(1,0,0)).magnitude() < tolerance);
+        assert(m.b.sub(vec(0,1,0)).magnitude() < tolerance);
+        assert(m.c.sub(vec(0,0,1)).magnitude() < tolerance);
+    }
+    {
+        //test 360 degree rotation about x
+        vec axis(1,0,0);
+        float angle = 2*M_PI;
+        matrix3 m(angle, axis);
+        assert(m.a.sub(vec(1,0,0)).magnitude() < tolerance);
+        assert(m.b.sub(vec(0,1,0)).magnitude() < tolerance);
+        assert(m.c.sub(vec(0,0,1)).magnitude() < tolerance);
+    }
+    {
+        //test 180 degree rotation about x
+        vec axis(1,0,0);
+        float angle = M_PI;
+        matrix3 m(angle, axis);
+        assert(m.a.sub(vec(1,0,0)).magnitude() < tolerance);
+        assert(m.b.sub(vec(0,-1,0)).magnitude() < tolerance);
+        assert(m.c.sub(vec(0,0,-1)).magnitude() < tolerance);
+    }
+    {
+        //test 180 degree rotation about y
+        vec axis(0,1,0);
+        float angle = M_PI;
+        matrix3 m(angle, axis);
+        assert(m.a.sub(vec(-1,0,0)).magnitude() < tolerance);
+        assert(m.b.sub(vec(0,1,0)).magnitude() < tolerance);
+        assert(m.c.sub(vec(0,0,-1)).magnitude() < tolerance);
+    }
 }
 
 void test_matrix3_identity()
