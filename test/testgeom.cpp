a35e1e00098ad1b160dcdd4fe1958030307a4946
Alex Foster <123afoster@gmail.com>
2024-03-19 23:52:28 -0700

    test vec2 operator[]

--- test/testgeom.cpp
+++ test/testgeom.cpp
@@ -7,6 +7,21 @@ constexpr float tolerance = 0.001;
 // float vec2 tests
 ////////////////////////////////////////////////////////////////////////////////
 
+void test_vec2_bracket()
+{
+    std::printf("testing vec2 operator[]\n");
+
+    vec2 v(1,2);
+
+    float &f1 = v[0],
+          &f2 = v[1];
+    float f3 = v[0],
+          f4 = v[1];
+    assert(f1 == 1);
+    assert(f2 == 2);
+    assert(f3 == 1);
+    assert(f4 == 2);
+}
 
 void test_vec2_iszero()
 {
@@ -508,6 +523,7 @@ testing geometry\n\
 ===============================================================\n"
     );
 
+    test_vec2_bracket();
     test_vec2_iszero();
     test_vec2_lerp();
 
