514a9cd90408d90f8e56863f46fc168e7777d4e4
Alex Foster <123afoster@gmail.com>
2024-04-29 22:06:33 -0700

    fix whitespace in hud maxcrosshairs

--- src/engine/render/hud.cpp
+++ src/engine/render/hud.cpp
@@ -147,7 +147,7 @@ namespace
         }
     }
 
-    const int maxcrosshairs  = 4;
+    const int maxcrosshairs = 4;
     std::array<Texture *, maxcrosshairs> crosshairs = { nullptr, nullptr, nullptr, nullptr };
 
     void loadcrosshair(const char *name, int i)
