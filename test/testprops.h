bb1aa67b505bbc9e75e28f760bd26489fd862df7
Alex Foster <123afoster@gmail.com>
2024-06-05 00:15:32 -0700

    header guard testprops & testshadowatlas

--- test/testprops.h
+++ test/testprops.h
@@ -1,1 +1,6 @@
+#ifndef TEST_PROPS_H_
+#define TEST_PROPS_H_
+
 extern void test_props();
+
+#endif
--- test/testshadowatlas.h
+++ test/testshadowatlas.h
@@ -1,1 +1,6 @@
+#ifndef TEST_SHADOWATLAS_H_
+#define TEST_SHADOWATLAS_H_
+
 extern void testpacknode();
+
+#endif
