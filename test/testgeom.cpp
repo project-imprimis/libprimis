f826f4d1fa0465f8d09caaba20be642f07a13a63
Alex Foster <123afoster@gmail.com>
2024-06-15 11:14:28 -0800

    fix vec4 div3 test

--- test/testgeom.cpp
+++ test/testgeom.cpp
@@ -1583,7 +1583,7 @@ namespace
         {
             assert(vec4<float>(3,4,0,0).div3(1) == vec4<float>(3,4,0,0));
             assert(vec4<float>(3,6,0,0).div3(3) == vec4<float>(1,2,0,0));
-            assert(vec4<float>(6,0,8,0).div3(2) == vec4<float>(3,0,6,0));
+            assert(vec4<float>(6,0,8,0).div3(2) == vec4<float>(3,0,4,0));
             assert(vec4<float>(2,4,6,8).div3(2) == vec4<float>(1,2,3,8));
         }
     }
