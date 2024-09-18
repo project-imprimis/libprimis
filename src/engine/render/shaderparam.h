0f1d64feed8db519588b6225801c35eb6e977e36
Alex Foster <123afoster@gmail.com>
2024-09-17 23:44:04 -0700

    GLint in UniformLoc

--- src/engine/render/shaderparam.h
+++ src/engine/render/shaderparam.h
@@ -4,7 +4,9 @@
 struct UniformLoc
 {
     const char *name, *blockname;
-    int loc, version, binding, stride, offset, size;
+    GLint loc;
+    int version, binding, stride, offset;
+    GLint size;
     const void *data;
     UniformLoc(const char *name = nullptr, const char *blockname = nullptr, int binding = -1, int stride = -1) : name(name), blockname(blockname), loc(-1), version(-1), binding(binding), stride(stride), offset(-1), size(-1), data(nullptr) {}
 };
