c5ba9032dd918705f06f5003d27c3a38d34bbc6b
Alex Foster <123afoster@gmail.com>
2024-10-10 20:53:14 -0700

    fix slot test order

--- test/testslot.cpp
+++ test/testslot.cpp
@@ -11,13 +11,6 @@ namespace
         assert(s.type() == Slot::SlotType_Octa);
     }
 
-    void test_slot_texturedir()
-    {
-        std::printf("testing slot::texturedir\n");
-        Slot s;
-        assert(s.texturedir() == std::string("media/texture"));
-    }
-
     void test_slot_name()
     {
         std::printf("testing slot::name\n");
@@ -26,6 +19,13 @@ namespace
         assert(s.name() == std::string("slot 1"));
         s.index = 123;
         assert(s.name() == std::string("slot 123"));
+    }
+
+    void test_slot_texturedir()
+    {
+        std::printf("testing slot::texturedir\n");
+        Slot s;
+        assert(s.texturedir() == std::string("media/texture"));
     }
 
     void test_decalslot_type()
@@ -54,8 +54,8 @@ testing slot functionality\n\
 ===============================================================\n"
     );
     test_slot_type();
-    test_slot_texturedir();
     test_slot_name();
+    test_slot_texturedir();
     test_decalslot_type();
     test_decalslot_name();
 }
