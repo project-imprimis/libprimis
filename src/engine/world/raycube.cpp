75d3a1ff0259f817414c2539a6de9cc2042edee9
Alex Foster <123afoster@gmail.com>
2025-02-19 19:14:37 -0800

    shadowray style fix

--- src/engine/world/raycube.cpp
+++ src/engine/world/raycube.cpp
@@ -488,7 +488,9 @@ float cubeworld::shadowray(const vec &o, const vec &ray, float radius, int mode,
         DOWNOCTREE(shadowent, );
 
         const cube &c = *lc;
-        ivec lo(x&(~0U<<lshift), y&(~0U<<lshift), z&(~0U<<lshift));
+        ivec lo(x&(~0U<<lshift),
+             y&(~0U<<lshift),
+             z&(~0U<<lshift));
 
         if(!(c.isempty()) && !(c.material&Mat_Alpha))
         {
