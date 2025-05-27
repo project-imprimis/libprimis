025b2ad35aeb408b95d19ae7654565e40a03f357
Alex Foster <123afoster@gmail.com>
2025-05-26 21:56:30 -0700

    explicit array length in aa edges

--- src/engine/render/aa.cpp
+++ src/engine/render/aa.cpp
@@ -349,12 +349,12 @@ namespace //internal functions incl. AA implementations
             bool smaasearchdatainited = false;
             bool smaaareadatainited = false;
 
-            static constexpr int orthoedges[][2] =
+            static constexpr int orthoedges[18][2] =
             {
                 {0, 0}, {3, 0}, {0, 3}, {3, 3}, {1, 0}, {4, 0}, {1, 3}, {4, 3},
                 {0, 1}, {3, 1}, {0, 4}, {3, 4}, {1, 1}, {4, 1}, {1, 4}, {4, 4}
             };
-            static constexpr int edgesdiag[][2] =
+            static constexpr int edgesdiag[18][2] =
             {
                 {0, 0}, {1, 0}, {0, 2}, {1, 2}, {2, 0}, {3, 0}, {2, 2}, {3, 2},
                 {0, 1}, {1, 1}, {0, 3}, {1, 3}, {2, 1}, {3, 1}, {2, 3}, {3, 3}
