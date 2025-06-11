b996296c29c0506b1719ec8b8e748213bc5cc16d
Alex Foster <123afoster@gmail.com>
2025-06-10 22:19:42 -0700

    document limitsky

--- src/engine/render/rendersky.h
+++ src/engine/render/rendersky.h
@@ -5,6 +5,15 @@ extern int skyshadow;
 
 extern void setexplicitsky(bool val);
 extern void drawskybox(bool clear = false);
+
+/**
+ * @brief Returns status of sky rendering.
+ *
+ * Does not affect state of the program.
+ *
+ * @return true if explicit sky is enabled, and skytexture is being used or edit is enabled
+ * @return false otherwise
+ */
 extern bool limitsky();
 
 #endif
