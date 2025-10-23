3ac34dfdbce9fae4466de2c275de89b051c26fc4
Alex Foster <123afoster@gmail.com>
2025-10-22 22:43:42 -0700

    document resethudshader()

--- src/engine/render/hud.h
+++ src/engine/render/hud.h
@@ -9,5 +9,11 @@ extern float conscale;
 extern void gl_drawmainmenu();
 extern void gl_drawhud(int crosshairindex, void(* hud2d)());
 extern void writecrosshairs(std::fstream& f);
+
+/**
+ * @brief Resets HUD shader and color.
+ *
+ * Sets the HUD shader color to white (1,1,1).
+ */
 extern void resethudshader();
 #endif
