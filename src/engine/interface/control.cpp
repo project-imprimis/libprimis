c14f672a38a5588ff1f838890f91773c9f16a6ed
Alex Foster <123afoster@gmail.com>
2025-11-30 23:38:00 -0800

    roll version string over to v0.60a

--- src/engine/interface/control.cpp
+++ src/engine/interface/control.cpp
@@ -162,7 +162,7 @@ int getclockmillis()
 //identification info about engine
 std::string enginestr()
 {
-    return "Libprimis v0.59a";
+    return "Libprimis v0.60a";
 }
 
 std::string enginebuilddate()
