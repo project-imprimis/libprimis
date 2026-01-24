d9c5f4cb2059069f5cdb46d7035857f04508981e
Alex Foster <123afoster@gmail.com>
2026-01-23 23:18:46 -0800

    document setupao()

--- src/engine/render/ao.h
+++ src/engine/render/ao.h
@@ -32,6 +32,16 @@ extern void cleanupao();
  * where the darkening of the scene is
  */
 extern void viewao();
+
+/**
+ * @brief Sets up the AO buffers
+ *
+ * The input buffers are scaled by the aoreduce variable. aoreduce scales the buffers
+ * by half for each increment.
+ *
+ * @param w the width of the buffers to set up
+ * @param h the height of the buffers to set up
+ */
 extern void setupao(int w, int h);
 
 #endif
