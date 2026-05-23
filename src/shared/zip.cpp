29f803594473953569459f6d728a555daa2c7b02
Alex Foster <123afoster@gmail.com>
2026-05-22 22:34:19 -0700

    document debugzip

--- src/shared/zip.cpp
+++ src/shared/zip.cpp
@@ -155,7 +155,7 @@ static bool findzipdirectory(FILE *f, zipdirectoryheader &hdr)
     return true;
 }
 
-static VAR(debugzip, 0, 0, 1);
+static VAR(debugzip, 0, 0, 1); /// toggles printing to console information about zip file actions
 
 static bool readzipdirectory(const char *archname, FILE *f, int entries, int offset, uint size, std::vector<zipfile> &files)
 {
