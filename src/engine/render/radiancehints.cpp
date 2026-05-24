9f977efaf724e5251cbc2af4246a2bcea4bb6bf1
Alex Foster <123afoster@gmail.com>
2026-05-23 23:39:03 -0700

    std::array in setupradiancehints()

--- src/engine/render/radiancehints.cpp
+++ src/engine/render/radiancehints.cpp
@@ -198,8 +198,8 @@ void setupradiancehints()
             glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
             glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
             glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_BORDER);
-            GLfloat border[4] = { 0.5f, 0.5f, 0.5f, 0 };
-            glTexParameterfv(GL_TEXTURE_3D, GL_TEXTURE_BORDER_COLOR, border);
+            std::array<GLfloat, 4> border = { 0.5f, 0.5f, 0.5f, 0 };
+            glTexParameterfv(GL_TEXTURE_3D, GL_TEXTURE_BORDER_COLOR, border.data());
         }
     }
     if(!rhfbo)
