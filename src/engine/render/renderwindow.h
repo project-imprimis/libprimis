199b21320fda421a16e71ddd144a95b808246888
Alex Foster <123afoster@gmail.com>
2026-06-20 23:04:16 -0700

    document renderwindow.h vars

--- src/engine/render/renderwindow.h
+++ src/engine/render/renderwindow.h
@@ -1,10 +1,13 @@
 #ifndef RENDERWINDOW_H_
 #define RENDERWINDOW_H_
 
-extern SDL_Window *screen;
-extern int scr_w, scr_h;
-extern int screenw, screenh;
-extern float loadprogress;
+extern SDL_Window *screen; /// handle to SDL object that manages the window
+extern int scr_w, /// width of window in pixels, as requested by user via cubescript
+           scr_h; /// height of window in pixels, as requested by user via cubescript
+extern int screenw, /// width of window in pixels, queried from SDL
+           screenh; /// height of window in pixels, queried from SDL
+extern float loadprogress; /// value between 0 and 1, sets size of progress loading bar
+extern bool inbetweenframes; /// returns true when renderer is not in the octarender() call, false if it is
 
 /**
  * @brief Restores gamma to default level
@@ -23,8 +26,6 @@ extern void cleargamma();
  * @param worstdiff difference between average and worst fps
  */
 extern void getfps(int &fps, int &bestdiff, int &worstdiff);
-
-extern bool inbetweenframes; /// returns true when renderer is not in the octarender() call, false if it is
 
 extern void renderbackground(const char *caption = nullptr, const Texture *mapshot = nullptr, const char *mapname = nullptr, const char *mapinfo = nullptr, bool force = false);
 extern void renderprogress(float bar, const char *text, bool background = false);
