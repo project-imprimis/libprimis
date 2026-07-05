592022bc9e7d4bd91bc041fd70df39a2f52b3cc9
Alex Foster <123afoster@gmail.com>
2026-07-05 10:57:37 -0400

    fix narrowing and const in vacollect std::hash

--- src/engine/render/vacollect.cpp
+++ src/engine/render/vacollect.cpp
@@ -461,8 +461,8 @@ class vacollect final
 
 int vacollect::addvert(const vertex &v)
 {
-    auto vechash = std::hash<vec>();
-    uint h = vechash(v.pos)&(hashsize-1);
+    const auto vechash = std::hash<vec>();
+    size_t h = vechash(v.pos)&(hashsize-1);
     for(int i = table[h]; i>=0; i = chain[i])
     {
         const vertex &c = verts[i];
