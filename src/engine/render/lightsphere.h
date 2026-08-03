577a35b026151520414c02272a89fb769b9c7009
Alex Foster <123afoster@gmail.com>
2026-08-02 23:54:11 -0700

    document lightsphere disable()

--- src/engine/render/lightsphere.h
+++ src/engine/render/lightsphere.h
@@ -6,6 +6,13 @@ namespace lightsphere
     extern void cleanup();
     extern void enable();
     extern void draw();
+
+    /**
+     * @brief Disables the lightsphere drawing mode.
+     *
+     * Reverts the lightsphere GL setup done by enable(). This should be called
+     * after lightsphere `draw()`.
+     */
     extern void disable();
 }
 
