852b3aa2de73f8719842efa8fd0bdc196356910e
Alex Foster <123afoster@gmail.com>
2026-09-15 09:04:02 -0800

    fix documentation for getwaterfog()

--- src/engine/render/water.h
+++ src/engine/render/water.h
@@ -51,7 +51,9 @@ extern const bvec &getwaterfallcolor(int mat);
  * There are four water materials; the mat parameter selects the water material
  * to query.
  *
- * @param mat which water to query (water1, water2, water3, water4)
+ * @param mat which water to query (0..3)
+ *
+ * @return water fog distance value
  */
 extern int getwaterfog(int mat);
 
